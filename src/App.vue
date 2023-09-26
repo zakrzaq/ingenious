6acf0e22820c83c7f4481adfd66831df0273454e
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-09-26 14:35:32 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -23,5 +23,4 @@ onMounted(async () => {
 </template>
 
 <style>
-@import "@/assets/index.css";
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap";
+import "@/assets/styles/index.scss";
 
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { store, key } from "./store";
 
-// import "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap";
 
 const app = createApp(App);
 app.use(store, key);
